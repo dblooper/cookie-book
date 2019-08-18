aa6782c9e68ebd9c99af1b30bcb2292dc9636400
dkoplenski <dkoplenski@gmail.com>
2019-08-18 16:09:22 +0200

    First commit

---
+++ src/main/java/com/cookie_book/CookieBookApplication.java
@@ -0,0 +1,13 @@
+package com.cookie_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookieBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cookie_book/CookieBookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cookie_book;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CookieBookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
